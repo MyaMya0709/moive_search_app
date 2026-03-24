8fa43ba85a0fab5558be40d930f98516679562fc
MyaMya0709 <chlehdgh96@gmail.com>
2026-03-24 23:42:54 +0900

    [Feat] UI 다듬기

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ async function fetchMovies(keyword) {
 
         if (data.Response === "False") {
             message.textContent = data.Error || "검색 결과가 없습니다.";
-            movieList.innerHTML = "";
+            renderEmptyState("에러가 발생했습니다.");
             return;
         }
 
@@ -46,51 +46,43 @@ async function fetchMovies(keyword) {
     catch (error) {
         console.error(error);
         message.textContent = "에러가 발생했습니다. 잠시 후 다시 시도해주세요.";
-        movieList.innerHTML = "";
+        renderEmptyState("에러가 발생했습니다.");
     }
-
-    // const mockData = [
-    // {
-    //    Title: "Avengers: Endgame",
-    //    Year: "2019",
-    //    Type: "movie",
-    //    Poster: "https://via.placeholder.com/300x450?text=Avengers",
-    // },
-    // {
-    //    Title: "Avengers: Infinity War",
-    //    Year: "2018",
-    //    Type: "movie",
-    //    Poster: "https://via.placeholder.com/300x450?text=Infinity+War",
-    // },
-    // ];
-
-    // message.textContent = `"${keyword}" 검색 결과`;
-    // renderMovies(mockData);
 }
 
 function renderMovies(movies) {
     movieList.innerHTML = "";
 
     movies.forEach((movie) => {
-        console.log(movie.Title, movie.Poster);
+        const poster =
+        movie.Poster && movie.Poster !== "N/A"
+            ? movie.Poster
+            : "https://placehold.co/300x450?text=No+Image";
 
-        const poster = 
-            movie.Poster && movie.Poster !== "N/A"
-                ? movie.Poster
-                : "https://placehold.co/300x450?text=No+Image"
         const movieCard = `
-            <article class="movie-card">
-                <img class="movie-poster"
-                    src="${poster}"
-                    alt="${movie.Title} 포스터"
-                    onerror="this.src='https://placehold.co/300x450?text=No+Image'"
-                />
-                <h3>${movie.Title}</h3>
-                <p>개봉년도: ${movie.Year}</p>
-                <p>타입: ${movie.Type}</p>
-            </article>
+        <article class="movie-card">
+            <img
+            class="movie-poster"
+            src="${poster}"
+            alt="${movie.Title} 포스터"
+            onerror="this.src='https://placehold.co/300x450?text=No+Image'"
+            />
+            <div class="movie-info">
+            <h3>${movie.Title}</h3>
+            <p>개봉년도: ${movie.Year}</p>
+            <p>타입: ${movie.Type}</p>
+            </div>
+        </article>
         `;
 
         movieList.innerHTML += movieCard;
-    })
+    });
+}
+
+function renderEmptyState(text) {
+    movieList.innerHTML = `
+    <div class="empty-box">
+        ${text}
+    </div>
+    `;
 }
